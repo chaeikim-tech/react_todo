25df60a921ac3c4c35408088502374ce5fca9a0d
eunju.kim <sb02348@hanmail.net>
2022-01-23 03:04:36 +0900

    React-hook-form Error message

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -27,23 +27,45 @@ import {useForm} from "react-hook-form";
     );
 } */
 
+interface IForm {
+    email: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    password1: string;
+  }
+
 function ToDoList() {
-    const { register, handleSubmit, formState } = useForm();
-    {/* handleSubmit => validation 기능 o */}
+    const { register, handleSubmit, formState: {errors}} = useForm<IForm>({
+        defaultValues:{
+            email: "@naver.com",
+        },
+    });
     const onValid = (data: any) => {
         console.log(data);
     };
-    console.log(formState.errors);
     return(
         <div>
             <form style={{display: "flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
-                <input {...register("email",{required: true})} placeholder='Email' />
-                <input {...register("firstName",{required: true})} placeholder='First Name' />
-                <input {...register("lastName",{required: true})} placeholder='Last Name' />
-                <input {...register("username",{required: true, minLength: 5})} placeholder='Username' />
-                <input {...register("password",{required: true, minLength: 10})} placeholder='Password' />
+                <input {...register("email",{
+                    required: "Email is required",
+                    pattern:{
+                        value:/^[A-Za-z0-9._%+-]+@naver.com$/,
+                        message:"Only naver.com emails allowed",
+                },
+                })} placeholder='Email' />
+                <span>{errors?.email?.message}</span>
+                <input {...register("firstName",{required: "Write here"})} placeholder='First Name' />
+                <span>{errors?.firstName?.message}</span>
+                <input {...register("lastName",{required: "Write here"})} placeholder='Last Name' />
+                <span>{errors?.lastName?.message}</span>
+                <input {...register("username",{required: "Write here", minLength: 5})} placeholder='Username' />
+                <span>{errors?.username?.message}</span>
+                <input {...register("password",{required: "Write here", minLength: 10})} placeholder='Password' />
+                <span>{errors?.password?.message}</span>
                 <input {...register("password1",{required: "Password is required",minLength:{value:10, message: "Your password is too short.",},})} placeholder='Password1' />
-                {/* HTML에 의지하는게 아니라 JavaScript를 사용해서 validation을 실행 => {...register({required:true})} */}
+                <span>{errors?.password1?.message}</span>
                 <button>Add</button>
             </form>
         </div>
