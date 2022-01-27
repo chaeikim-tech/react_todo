f9aaca1d282c129b9cb59abe522ee4a6a507b144
eunju.kim <sb02348@hanmail.net>
2022-01-27 18:18:03 +0900

    ToDo enumerable

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -3,17 +3,27 @@ import { atom, selector } from "recoil";
 // atom은 그냥 배열.
 //selector는 state를 가져다가 뭔가를 return함
 
-type categories = "TO_DO" | "DOING" | "DONE";
+export enum Categories {
+    "TO_DO" = "TO_DO",
+    "DOING" = "DOING",
+    "DONE" = "DONE",
+}
+
+//enum = enumerable = 프로그래머를 도와주기 위해서 일련의 숫자를 문자로 표현해줌.
+//열거형으로 이름이 있는 상수들의 집합을 정의할 수 있습니다.
+//열거형을 사용하면 의도를 문서화 하거나 구분되는 사례 집합을 더 쉽게 만들수 있습니다.
+//TypeScript는 숫자와 문자열-기반 열거형을 제공합니다.
+//https://www.typescriptlang.org/ko/docs/handbook/enums.html
 
 export interface IToDo {
     text: string;
     id: number;
-    category: categories;
+    category: Categories;
 };
 
-export const categoryState = atom<categories>({
+export const categoryState = atom<Categories>({
     key:"category",
-    default: "TO_DO",
+    default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDo[]>({
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRecoilValue, useRecoilState } from "recoil";
-import { toDoSelector, categoryState } from "../atoms";
+import { toDoSelector, categoryState, Categories } from "../atoms";
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
@@ -16,9 +16,9 @@ function ToDoList() {
             <h1>To Dos</h1>
             <hr />
             <select value={category} onInput={onInput}>
-                <option value="TO_DO">TO DO</option>
-                <option value="DOING">Doing</option>
-                <option value="DONE">Done</option>
+                <option value={Categories.TO_DO}>TO DO</option>
+                <option value={Categories.DOING}>Doing</option>
+                <option value={Categories.DONE}>Done</option>
             </select>
             <CreateToDo />
             {toDos?.map((toDo) => (
