16112fd43905b28c7cc5a729780ce625a5da33ec
eunju.kim <sb02348@hanmail.net>
2022-01-27 14:51:43 +0900

    ToDo select state example

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -21,6 +21,11 @@ function ToDoList() {
                 <option value="DONE">Done</option>
             </select>
             <CreateToDo />
+            {/* bad example 
+            {category === "TO_DO" && toDo.map((aToDo) => <ToDo key={aToDo.id} {...aToDo} />)}
+            {category === "DOING" && doing.map((aToDo) => <ToDo key={aToDo.id} {...aToDo} />)}
+            {category === "DONE" && done.map((aToDo) => <ToDo key={aToDo.id} {...aToDo} />)}
+ */}
             {toDos?.map((toDo) => (
                 <ToDo key={toDo.id} {...toDo} />
             ))}
